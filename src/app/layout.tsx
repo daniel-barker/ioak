e0e8d332240df3c9bc8756e251256d4ec2814fd5
dan barker <dbarker16@gmail.com>
2025-07-19 21:09:42 -0400

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,8 @@ const oswald = Oswald({
 
 export const metadata: Metadata = {
   title: "Ingredients of a Killer | M.C. Lawrence",
-  description: "He's the boy next door—with blood on his apron. A psychological thriller by M.C. Lawrence.",
+  description:
+    "He's the boy next door—with blood on his apron. A psychological thriller by M.C. Lawrence.",
 };
 
 export default function RootLayout({
@@ -25,6 +26,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${bebasNeue.variable} ${oswald.variable} antialiased bg-black text-white`}
       >
